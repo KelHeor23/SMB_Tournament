6cb77fea6d4976ff0998cef47035421890138d0e
KelHeor23 <kelheor23@gmail.com>
2020-01-05 16:26:29 +0300

    Bla-bla-bla

--- filling.cpp
+++ filling.cpp
@@ -1,11 +1,22 @@
 #include "filling.h"
 #include "ui_filling.h"
+#include "QtSql"
 
 Filling::Filling(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Filling)
 {
     ui->setupUi(this);
+
+    //QTableView view;
+    QSqlTableModel model;
+
+    model.setTable("Person");
+    model.select();
+    model.setEditStrategy(QSqlTableModel::OnFieldChange);
+
+    ui->tableView->setModel(&model);
+    ui->tableView->show();
 }
 
 Filling::~Filling()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
+#include "QtSql"
 
 #include <QApplication>
+#include <QTableView>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -8,5 +11,31 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     ///a
+    ///
+    QSqlDatabase sdb = QSqlDatabase::addDatabase("QSQLITE");
+    sdb.setDatabaseName("TournamentData.sqlite");
+
+    if (!sdb.open())
+    {
+       // qDebug() << "Что-то не так с соединением!";
+        QMessageBox::critical(nullptr, QObject::tr("Cannot open database"),
+            QObject::tr("Unable to establish a database connection.\n"
+                        "This example needs SQLite support. Please read "
+                        "the Qt SQL driver documentation for information how "
+                        "to build it.\n\n"
+                        "Click Cancel to exit."), QMessageBox::Cancel);
+        return -1;
+    }
+
+    QTableView view;
+    QSqlTableModel model;
+
+    model.setTable("Person");
+    model.select();
+    model.setEditStrategy(QSqlTableModel::OnRowChange);
+
+    view.setModel(&model);
+    view.show();
+
     return a.exec();
 }
